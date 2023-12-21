9f4b96cfce439018f4e4d838bd1506ce336a0473
RushiPardeshi <rushipardeshi317@gmail.com>
2023-12-20 22:41:31 -0500

    Got customer.py/html working

--- api/user/customer.py
+++ api/user/customer.py
@@ -6,19 +6,29 @@ from db import db
 
 customer = Blueprint('customer', __name__)
 
+cust = []
+
 # get customer with user id
-@customer.route("/user/<cust_id>", methods=['GET'])
-def getCustomerByUserID(cust_id):
+@customer.route("/user_login", methods=['GET'])
+def getCustomerByUserID():
+    name = request.args.get('name')
+    address = request.args.get('address')
+    # name = request.form['name']
+    # address = request.form['address']
     result = db.session.execute(text('''Select * 
                                      from customer c  
-                                     where c.cust_id = :id'''), params={'id': cust_id}).fetchall()
-    results = []
-    for r in result:
-        dic = {}
-        dic['name'] = r[1]
-        dic['address'] = r[2]
-        results.append(dic)
-    return results
+                                     where c.name = :name and c.bill_addr = :address'''), params={'name': name, 'address': address}).fetchone()
+    
+    # dic = {'name': result[1], 'address': result[2]}
+    curr_cust = {"cust_id":result[0], "name":name, "bill_addr": address}
+    if curr_cust not in cust:
+        cust.append(curr_cust)
+    return render_template('customer.html', name=name,cust_id=result[0])
+
+@customer.route("/user/<cust_id>")
+def customerDetails(cust_id):
+    
+    return render_template("customer.html",)
 
 # get all locations for a customer
 @customer.route("/user/<cust_id>/locations", methods=['GET'])
@@ -38,6 +48,24 @@ def getLocationsByUserID(cust_id):
         dic['zipcode'] = r[7]
         results.append(dic)
     print(result)
+    # print("aaya yaha")
+    return results
+
+@customer.route("/user/<cust_id>/locations/<loc_id>/devices", methods=['GET'])
+def getDevicesByLoc(cust_id, loc_id):
+    result = db.session.execute(text('''Select d.dev_id, m.model_name, m.model_type 
+                                     from device d 
+                                     natural join service_loc sl 
+                                     natural join model m
+                                     where sl.cust_id = :id and sl.loc_id = :loc_id'''), params={'id': cust_id, 'loc_id': loc_id}).fetchall()
+    results = []
+    for r in result:
+        dic = {}  
+        dic['dev_id'] = r[0]
+        dic['model_name'] = r[1]
+        dic['model_type'] = r[2]
+        results.append(dic)
+    
     return results
 
 # get devices list with user id
@@ -48,19 +76,22 @@ def getDevicesByUserID(cust_id):
                                      natural join service_loc sl 
                                      natural join model m
                                      where sl.cust_id = :id'''), params={'id': cust_id}).fetchall()
-    results = []
-    for r in result:
-        dic = {}
-        dic['dev_id'] = r[0]
-        dic['model_name'] = r[1]
-        dic['model_type'] = r[2]
+    # results = []
+    # for r in result:
+    #     dic = {}
+    #     dic['dev_id'] = r[0]
+    #     dic['model_name'] = r[1]
+    #     dic['model_type'] = r[2]
     return result
+    # return render_template('model.html')
+    
 
 # add user
-@customer.route("/user", methods=['POST'])
+@customer.route("/user_register", methods=['GET'])
 def addLocation():
     try:
-        payload = request.get_json()
+        print('working')
+        payload = request.form
         cust_id = getNextCustId()
         addr = payload['addr']
         name = payload['name']
@@ -68,18 +99,20 @@ def addLocation():
         db.session.execute(text('''Insert into customer(cust_id, name, bill_addr) values 
                                 (:cust_id, :name, :addr)'''), params)
         db.session.commit()
-        return jsonify({'status': 'error', 'msg': 'Record added successfully'})
+        # return jsonify({'status': 'error', 'msg': 'Record added successfully'})
+        return render_template('login.html')
         
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
 
 # delete user
-@customer.route("/user/<cust_id>", methods=['DELETE'])
+@customer.route("/user/<cust_id>/del", methods=['DELETE'])
 def deleteDevice(cust_id):
     try:
         db.session.execute(text("Delete from customer where cust_id=:id"), params={'id': cust_id})
         db.session.commit()
-        return jsonify({'status': 'error', 'msg': 'Record deleted successfully'})
+        # return jsonify({'status': 'error', 'msg': 'Record deleted successfully'})
+        return render_template("login.html")
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
     
--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from api.views.view import view
 from db import init_app, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rushipardeshi:animefreak@localhost/project'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:animefreak@localhost/project'
 app.register_blueprint(devices)
 app.register_blueprint(serviceLocation)
 app.register_blueprint(customer)
@@ -24,7 +24,7 @@ service_locations = [
 
 smart_devices = {
     "refrigerator": ["Bosch 800", "Samsung XYZ"],
-    "AC": ["GE 4500", "Carrier 2000"],
+    "AC": ["GE 4500", "Carrier 2000"]
     # Add more devices for each type
 }
 
@@ -36,59 +36,55 @@ cust = [
 def index():
     return render_template('index.html')
 
-@app.route('/add_location', methods=['POST'])
-def add_location():
-    # Get data from the form
-    loc_id = request.form['loc_id']
-    addr = request.form['addr']
-    zipcode = request.form['zipcode']
-    # Add new location to the list
-    service_locations.append({"loc_id": loc_id, "addr": addr, "zipcode": zipcode})
-    return render_template('index.html', locations=service_locations)
+# @app.route('/add_location', methods=['POST'])
+# def add_location():
+#     # Get data from the form
+#     loc_id = request.form['loc_id']
+#     addr = request.form['addr']
+#     zipcode = request.form['zipcode']
+#     # Add new location to the list
+#     service_locations.append({"loc_id": loc_id, "addr": addr, "zipcode": zipcode})
+#     return render_template('index.html', locations=service_locations)
 
-@app.route('/remove_location/<loc_id>')
-def remove_location(loc_id):
-    # Remove the location with the given loc_id
-    for loc in service_locations:
-        if loc['loc_id'] == loc_id:
-            service_locations.remove(loc)
-            break
-    return render_template('index.html', locations=service_locations, devices=smart_devices)
+# @app.route('/remove_location/<loc_id>')
+# def remove_location(loc_id):
+#     # Remove the location with the given loc_id
+#     for loc in service_locations:
+#         if loc['loc_id'] == loc_id:
+#             service_locations.remove(loc)
+#             break
+#     return render_template('index.html', locations=service_locations, devices=smart_devices)
 
-@app.route('/add_device', methods=['POST'])
-def add_device():
-    # Get data from the form
-    device_type = request.form['device_type']
-    selected_model = request.form['selected_model']
+# @app.route('/add_device', methods=['POST'])
+# def add_device():
+#     # Get data from the form
+#     device_type = request.form['device_type']
+#     selected_model = request.form['selected_model']
 
-    # Add new device to the list
-    smart_devices[device_type].append(selected_model)
-    return render_template('index.html', locations=service_locations, devices=smart_devices)
-
-@app.route('/add_location', methods=['POST'])
-def add_customer():
-    cust_name = request.form['cust_name']
-    cust_addr = request.form['cust_addr']
-
-    cust.append()
+#     # Add new device to the list
+#     smart_devices[device_type].append(selected_model)
+#     return render_template('index.html', locations=service_locations, devices=smart_devices)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    name = request.form.get("name")
-    addr = request.form.get("address")
-    result = db.session.execute(text(f"select * from customer where customer.name={name} and customer.bill_addr={addr}"))
-    ans = result.fetchall()
-    
-    if len(ans) > 1:
-      cust_id = ans[0][0]
-      cust_name = ans[0][1]
-    print(ans)
+    return render_template("login.html")
 
-      return render_template("model.html")
-
-@app.route('/{cust[cust_id]}/model', methods = ['GET','POST'])
-def model():
+@app.route('/register', methods=['GET'])
+def register():
     return render_template("register.html")
+
+# @app.route('/{cust[cust_id]}/model', methods = ['GET','POST'])
+# def model():
+#     name = request.form.get("name")
+#     addr = request.form.get("address")
+#     result = db.session.execute(text(f"select * from customer where customer.name='{name}' and customer.bill_addr='{addr}'"))
+#     ans = result.fetchall()
+    
+#     cust[cust_id] = ans[0][0]
+#     cust[cust_name] = ans[0][1]
+#     print(ans)
+
+#     return render_template("model.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
